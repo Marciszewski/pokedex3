5beedfb591ecceb17b7e011f5100e424f681b435
Marciszewski <marciszewskimarek@gmail.com>
2017-07-24 17:06:15 +0100

    Dodałem searchBar

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  pokedex3
+//
+//  Created by Macbook on 15.07.2017.
+//  Copyright © 2017 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class PokeCellCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Macbook on 13.07.2017.
+//  Copyright © 2017 Macbook. All rights reserved.
+//
+
+import Foundation
